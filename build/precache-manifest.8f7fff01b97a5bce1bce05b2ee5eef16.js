2aa55f0b98d6f19b92189a36404aed37e346c2bd
songqian-360che <qian.song@360che.com>
2019-03-20 10:29:07 +0800

    chore: 重构脚手架

---
+++ build/precache-manifest.8f7fff01b97a5bce1bce05b2ee5eef16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49743d2d1abbde49e0912a0984af41ce",
+    "url": "./static/media/loading.49743d2d.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5dae1b35201cc4ee9066",
+    "url": "./static/js/main.067ef017.chunk.js"
+  },
+  {
+    "revision": "16ca79ed5318e70a2470",
+    "url": "./static/js/2.6c2ca3ee.chunk.js"
+  },
+  {
+    "revision": "5dae1b35201cc4ee9066",
+    "url": "./static/css/main.ced90851.chunk.css"
+  },
+  {
+    "revision": "a4389e5103287f1876470d5f1f747f8f",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c646563807eb0c4de86a61e6a9a3dd8a.js"
+  "./precache-manifest.8f7fff01b97a5bce1bce05b2ee5eef16.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
